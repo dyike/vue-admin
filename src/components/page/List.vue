865e9db7fc87d93a97fcb679547691f3409ec8ec
ityike <yuanfeng634@gmail.com>
2017-03-20 22:32:13 +0800

    list

--- src/components/page/List.vue
+++ src/components/page/List.vue
@@ -1,73 +1,155 @@
 <template>
-  <el-table
-    :data="tableData"
-    border
-    style="width: 100%">
-    <el-table-column
-      label="日期"
-      width="180">
-      <template scope="scope">
-        <el-icon name="time"></el-icon>
-        <span style="margin-left: 10px">{{ scope.row.date }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column
-      label="姓名"
-      width="180">
-      <template scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag>{{ scope.row.name }}</el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-    <el-table-column label="操作">
-      <template scope="scope">
-        <el-button
-          size="small"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-          size="small"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+    <div class="table">
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item><i class="el-icon-menu"></i> 表格</el-breadcrumb-item>
+                <el-breadcrumb-item>常用表格</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="top-search">
+          <el-form :inline="true" :model="formInline" class="demo-form-inline">
+            <el-form-item label="姓名">
+              <el-input v-model="formInline.name" placeholder="姓名"></el-input>
+            </el-form-item>
+            <el-form-item label="地址">
+              <el-select v-model="formInline.region" placeholder="地址">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+            </el-form-item>
+              <el-form-item label="日期">
+                <el-col :span="11">
+                  <el-date-picker type="date" placeholder="选择日期" v-model="formInline.date1" style="width: 100%;"></el-date-picker>
+                </el-col>
+                <el-col class="line" :span="2">-</el-col>
+                <el-col :span="11">
+                  <el-time-picker type="fixed-time" placeholder="选择时间" v-model="formInline.date2" style="width: 100%;"></el-time-picker>
+                </el-col>
+              </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">查询</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+
+        <div class="top-button">
+        </div>
+
+        <el-table :data="tableData" border style="width: 100%">
+            <el-table-column prop="date" label="日期" sortable width="150">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="120">
+            </el-table-column>
+            <el-table-column prop="address" label="地址" :formatter="formatter">
+            </el-table-column>
+            <el-table-column prop="tag" label="标签" width="120"
+                    :filters="[{ text: '家', value: '家' }, { text: '公司', value: '公司' }]"
+                    :filter-method="filterTag">
+                <template scope="scope">
+                    <el-tag :type="scope.row.tag === '家' ? 'primary' : 'success'" close-transition>{{scope.row.tag}}
+                    </el-tag>
+                </template>
+            </el-table-column>
+            <el-table-column label="操作" width="180">
+                <template scope="scope">
+                    <el-button size="small"
+                            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+
+                    <el-button size="small" type="danger"
+                            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <div class="pagination">
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="currentPage"
+              :page-sizes="[10, 20, 50, 100, 200, 300, 400]"
+              :page-size="100"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="400">
+            </el-pagination>
+        </div>
+    </div>
 </template>
 
 <script>
   export default {
     data() {
       return {
+        formInline: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: ''
+        },
         tableData: [{
           date: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '家'
         }, {
           date: '2016-05-04',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
+          address: '上海市普陀区金沙江路 1517 弄',
+          tag: '家'
         }, {
           date: '2016-05-01',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
+          address: '上海市普陀区金沙江路 1519 弄',
+          tag: '公司'
         }, {
           date: '2016-05-03',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }]
+          address: '上海市普陀区金沙江路 1516 弄',
+          tag: '公司'
+        }],
+        currentPage: 2,
+        input2: ''
       }
     },
     methods: {
+      onSubmit() {
+        console.log('submit!');
+      },
+      formatter(row, column) {
+        return row.address;
+      },
+      filterTag(value, row) {
+        return row.tag === value;
+      },
       handleEdit(index, row) {
         console.log(index, row);
       },
       handleDelete(index, row) {
         console.log(index, row);
+      },
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        this.currentPage = val;
+        console.log(`当前页: ${val}`);
+      },
+      handleIconClick(ev) {
+        console.log(ev);
       }
     }
   }
 </script>
+
+<style scoped>
+  .top-search {
+    position: relative;
+    padding-top: 8px;
+  }
+
+  .top-button {
+    position: relative;
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+</style>
+
