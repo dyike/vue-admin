408d1e448a1d9973f1057e75875942741d95ecbe
ityike <yuanfeng634@gmail.com>
2017-12-05 17:56:05 +0800

    css to scss

--- src/components/page/About.vue
+++ src/components/page/About.vue
@@ -49,49 +49,54 @@
     }
 </script>
 
-<style scoped>
-    .va-doc {
-        width:100%;
-        max-width: 980px;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
-    }
-    .va-doc h3 {
+<style scoped lang="scss">
+//colors
+$color_wild_sand_approx: #f5f5f5;
+$color_iron_approx: #d8d8d8;
+$white: #fff;
+$color_alto_approx: #ddd;
+$color_gallery_approx: #eee;
+
+.va-doc {
+    width: 100%;
+    h3 {
         padding: 9px 10px 10px;
         margin: 0;
         font-size: 14px;
         line-height: 17px;
-        background-color: #f5f5f5;
-        border: 1px solid #d8d8d8;
+        background-color: $color_wild_sand_approx;
+        border: 1px solid $color_iron_approx;
         border-bottom: 0;
         border-radius: 3px 3px 0 0;
     }
-    .va-doc article {
+    article {
         padding: 45px;
         word-wrap: break-word;
-        background-color: #fff;
-        border: 1px solid #ddd;
+        background-color: $white;
+        border: 1px solid $color_alto_approx;
         border-bottom-right-radius: 3px;
         border-bottom-left-radius: 3px;
+        h1 {
+            font-size: 32px;
+            padding-bottom: 10px;
+            margin-bottom: 15px;
+            border-bottom: 1px solid $color_alto_approx;
+        }
+        h2 {
+            margin: 24px 0 16px;
+            font-weight: 600;
+            line-height: 1.25;
+            padding-bottom: 7px;
+            font-size: 24px;
+            border-bottom: 1px solid $color_gallery_approx;
+        }
+        p {
+            margin-bottom: 15px;
+            line-height: 1.5;
+        }
+        .el-checkbox {
+            margin-bottom: 5px;
+        }
     }
-    .va-doc article h1 {
-        font-size:32px;
-        padding-bottom: 10px;
-        margin-bottom: 15px;
-        border-bottom: 1px solid #ddd;
-    }
-    .va-doc article h2 {
-        margin: 24px 0 16px;
-        font-weight: 600;
-        line-height: 1.25;
-        padding-bottom: 7px;
-        font-size: 24px;
-        border-bottom: 1px solid #eee;
-    }
-    .va-doc article p {
-        margin-bottom: 15px;
-        line-height: 1.5;
-    }
-    .va-doc article .el-checkbox {
-        margin-bottom: 5px;
-    }
+}
 </style>
